cdb3f8bb121196ef81d831cd658bc22dcd158f3e
Denizhan Şahin <denizhansahin@yaani.com>
2024-11-17 14:41:04 +0300

    first commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './typeorm/entities/User';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot({ //veya getCongig
+    type: 'sqlite',
+    database: 'database3.sqlite',
+    synchronize: true,
+    logging: false,
+    entities: [User],
+    migrations: [],
+    subscribers: [],
+  }),],
   controllers: [AppController],
   providers: [AppService],
 })
